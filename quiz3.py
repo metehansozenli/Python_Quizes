4e9c71aa24123dce7a6c72bbda6b7d96ed8bf2a6
Metehan SOZENLI <92057823+metehansozenli@users.noreply.github.com>
2022-02-16 00:13:22 +0300

    Update quiz3.py

--- quiz3.py
+++ quiz3.py
@@ -5,3 +5,13 @@ sayi= int(input("Kaclar tablosu yapilacak: "))
 for m in range(11):
     print(sayi,"x",m,"=",m*sayi)
     m += 1
+
+#Ekstra Cozum
+"""
+m=0
+sayi=int(input("Hangi sayinin carpim tablosunu istiyorsunuz: "))
+while(m<10):
+    print(m*sayi)
+    m += 1
+ 
+"""
