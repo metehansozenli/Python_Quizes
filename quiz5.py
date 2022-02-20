70ba72a91586e18764578330cfcc31308e9426ba
Metehan SÖZENLİ <92057823+metehansozenli@users.noreply.github.com>
2022-02-21 00:15:48 +0300

    Update quiz5.py

--- quiz5.py
+++ quiz5.py
@@ -2,3 +2,13 @@
 "once.txt" adlı dosyayı python ile okuyup içinceki metinlerin tamamını her bir karakterden sonra bir boşluk olacak şekilde düzeleyerek "sonra.txt" adlı 
 dosyaya yazacak python kodunu yazınız.
 """
+with open ("once.txt","r") as file:
+    okunan = file.read()
+    okunan2 = ""
+
+    for i in okunan:
+        okunan2 = okunan2 + " " + i
+
+with open ("sonra.txt","w") as file_write:
+    file_write.write(okunan2)
+    
