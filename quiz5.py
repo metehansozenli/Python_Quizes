e2fb01c92018c6f8c4f5247fb9641bfda91e6841
Metehan SÖZENLİ <92057823+metehansozenli@users.noreply.github.com>
2022-02-20 23:20:34 +0300

    Create quiz5.py

---
+++ quiz5.py
@@ -0,0 +1,2 @@
+""" "once.txt" adlı dosyayı python ile okuyup içinceki metinlerin tamamını her bir karakterden sonra bir boşluk olacak şekilde düzeleyerek 
+"sonra.txt" adlı dosyaya yazacak python kodunu yazınız """
